3fe5321e9e3be84cbd92268269406510c744e207
unnatisrivastava952 <rajeshku6758@gmail.com>
2025-04-02 16:08:42 +0530

    Update team.py

--- team.py
+++ team.py
@@ -4,9 +4,7 @@ st.set_page_config(page_title="Team Page", layout="wide")
 st.markdown("# Meet the Team")
 st.markdown("""
 Our team consists of experts in bioinformatics, machine learning, and web development.  
-- **Alice Smith** – Data Scientist  
-- **Bob Johnson** – ML Engineer  
-- **Carol Lee** – Bioinformatician  
-
-Feel free to reach out to us at [support@example.com](mailto:support@example.com).
+Dr.Shailesh Kumar - Staff Scientist (NIPGR) (http://nipgr.ac.in/research/dr_shailesh.php)
+Unnati Srivastva - Bioinformatics Student (srivastavaunnati93@gmail.com)
+Feel free to reach out to us at [support-http://nipgr.ac.in/research/dr_shailesh.php](mailto:srivastavaunnati93@gmail.com).
 """)
