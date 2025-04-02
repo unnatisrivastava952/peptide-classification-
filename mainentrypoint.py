f7aa0c1a0ec6d8bd8c1fb7652087684d1a676c96
unnatisrivastava952 <rajeshku6758@gmail.com>
2025-04-02 16:24:34 +0530

    Update mainentrypoint.py

--- mainentrypoint.py
+++ mainentrypoint.py
@@ -9,7 +9,7 @@ app = MultiApp()
 
 # Add all your applications here
 app.add_app("https://github.com/unnatisrivastava952/peptide-classification-/blob/main/app2.py",main_app.app)      # Your peptide classification page
-app.add_app("User Manual", manual.app)       # The manual page
+app.add_app("https://github.com/unnatisrivastava952/peptide-classification-/blob/main/manual.py", manual.app)       # The manual page
 app.add_app("Team", team.app)                # The team page
 
 app.run()
