9ef780289adbeb2a084d5d14f97dd742ee3048ef
unnatisrivastava952 <rajeshku6758@gmail.com>
2025-04-07 16:56:02 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -211,4 +211,5 @@ elif page == "🧬 Prediction":
                 st.error(f"⚠️ Unexpected error: {e}")
 
 # ----- Footer -----
-st.markdown("<hr><center><small>Built with using Streamlit and Plotly</small></center>", unsafe_allow_html=True).
+st.markdown("<hr><center><small>Built with using Streamlit and Plotly</small></center>", unsafe_allow_html=True)
+
