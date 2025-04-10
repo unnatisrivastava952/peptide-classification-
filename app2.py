e2946c228541b52c79cbcd6c3ceb898ccc3a9310
unnatisrivastava952 <rajeshku6758@gmail.com>
2025-04-10 11:36:25 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -111,7 +111,7 @@ elif page == "👨‍🔬 Team":
     with col1:
         st.image("https://github.com/unnatisrivastava952/peptide-classification-/blob/main/dr_shailesh.jpg", caption="Dr. Shailesh Kumar", width=200)
     with col2:
-        st.image("", caption="Unnati Srivastava", width=200)
+        st.image("https://github.com/unnatisrivastava952/peptide-classification-/blob/main/unnati.jpg", caption="Unnati Srivastava", width=200)
 
 # ----- Prediction Page -----
 elif page == "🧬 Prediction":
